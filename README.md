47f17671fc2bf1b5b173dc43fee628c51477495b
xuyuan23 <643854343@qq.com>
2023-07-31 14:32:19 +0800

    docs: Add verify way
    add verify method to bard proxy

--- README.md
+++ README.md
@@ -24,3 +24,7 @@ docker run -p 8671:8671 -e BARD_PROXY_API_KEY=YOUR-BARD-KEY bard_proxy
 2. start python project
 > python main.py
 
+
+## verify
+
+> curl -X POST -H "Content-Type: application/json" -d '{"input": "hello"}' http://localhost:8671/api/bard/v1/chat/completions
--- main.py
+++ main.py
@@ -10,15 +10,15 @@ app = Flask(__name__)
 token = os.getenv("BARD_PROXY_API_KEY", "Not config")
 
 
-@app.route('/api/openai/v1/chat/completions', methods=['POST'])
+@app.route("/api/bard/v1/chat/completions", methods=["POST"])
 def do_request():
-    print(f'token={token}')
-    input_text = request.json.get('input')
+    print(f"token={token}")
+    input_text = request.json.get("input")
     response = bardapi.core.Bard(token).get_answer(input_text)
-    if response and response.get('content'):
-        return response['content']
+    if response and response.get("content"):
+        return response["content"]
     return f"bard request error: response={str(response)}"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8671)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8671)
